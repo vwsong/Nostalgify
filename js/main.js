d2231bd9f049304769b32042e7507dd3f1b47a73
Vincent Song <vincent.wsong@gmail.com>
2016-02-20 18:51:49 -0500

    updated gh pages1

--- js/main.js
+++ js/main.js
@@ -40,7 +40,7 @@ function getUserStuff(delay) {
     var accessToken = accessKey;
     console.log(accessToken);
     spotifyApi.setAccessToken(accessToken);
-    spotifyApi.getUserPlaylists({limit: 30, offset: 20})
+    spotifyApi.getUserPlaylists({limit: 30, offset: 0})
         .then(function (data) {
             parsePlaylists(spotifyApi, data);
         }, function (err) {
